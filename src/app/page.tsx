5cd9bcdbbc8f5061ba31f6e6fd9b4664666494f4
Markus Wedler <markuswedler8@gmail.com>
2023-11-02 20:59:20 +1000

    Fix menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <main className="container pl-4 pr-4 sm:pl-8 sm:pr-8">
-      main
-      <div className="container bg-black">asd</div>
+      home
     </main>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from "react"
-import { HiOutlineMenuAlt2 } from "react-icons/hi"
+import { HiMenuAlt4 } from "react-icons/hi"
 
 export default () => {
   const [menuOpened, setMenuOpened] = useState(false)
@@ -15,13 +15,13 @@ export default () => {
           <div className="md:hidden">
             {/* menu btn */}
             <button onClick={() => setMenuOpened(true)} className="flex">
-              <HiOutlineMenuAlt2 className="h-5 w-5" />
+              <HiMenuAlt4 className="h-5 w-5" />
             </button>
           </div>
         </div>
       </header>
 
-      <menu data-state={menuOpened ? "open" : "closed"} style={{ pointerEvents: menuOpened ? "unset" : "none" }} className="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fill-mode-forwards">
+      <menu data-state={menuOpened ? "open" : "closed"} style={{ pointerEvents: menuOpened ? "unset" : "none", visibility: menuOpened ? "unset" : "hidden", transition: "visibility", transitionDelay: menuOpened ? "unset" : ".2s" }} className="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fill-mode-forwards">
         {/* bg */}
         <div onClick={() => setMenuOpened(false)} className="fixed inset-0 z-50" />
         {/* menu */}
@@ -36,7 +36,7 @@ export default () => {
             <span className="font-bold">Hello</span>
           </a>
           {/* close menu btn */}
-          <button onClick={() => setMenuOpened(false)} data-state={menuOpened ? "open" : "closed"} className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+          <button onClick={() => setMenuOpened(false)} data-state={menuOpened ? "open" : "closed"} className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 data-[state=open]:bg-secondary">
             <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4">
               <path d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" />
             </svg>
