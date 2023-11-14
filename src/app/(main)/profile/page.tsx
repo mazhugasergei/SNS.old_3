14711ab2b320aa04e51633fbe574f69091349e02
Markus Wedler <markuswedler8@gmail.com>
2023-11-14 17:53:08 +1000

    Improve profile page

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,13 +1,24 @@
 "use client"
 import { RootState } from "@/store/store"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 
 export default () => {
+  const router = useRouter()
+  const [loggedIn, setLoggedIn] = useState(false)
+  const auth = useSelector((state: RootState) => state.user.auth)
   const fullname = useSelector((state: RootState) => state.user.fullname)
   const bio = useSelector((state: RootState) => state.user.bio)
   const created = useSelector((state: RootState) => state.user.created)
 
-  return (
+  // redirect if logged out
+  useEffect(()=>{
+    if(auth === false) router.push("/")
+    else if(auth) setLoggedIn(true)
+  }, [auth])
+
+  return loggedIn && (
     <>
       <div className="contianer border rounded-md p-10">
         <h1 className="text-3xl font-bold mb-2">{ fullname }</h1>
